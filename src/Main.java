42fad8581562af586e24031273a4692ead3d814f
valdez <leonardovaldez.sn@gmail.com>
2026-02-08 12:52:31 -0300

    add inicialização de dados da cliente

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main() {
+        String name = "Gabriela Zanotti";
+        String accountType = "Conta Corrente";
+        double initialBalance = 3000;
+
+        System.out.println("--------------------");
+        System.out.println("Dados iniciais da cliente:");
+        System.out.println("\nNome: " + name);
+        System.out.println("Tipo de conta: " + accountType);
+        System.out.println("Saldo inicial: R$ " +  initialBalance);
+        System.out.println("\n--------------------");
+
+    }
+}
